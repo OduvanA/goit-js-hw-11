175c661e2e87420925258cea3351a3e398648728
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-03-01 01:45:59 -0600

    hw11 mainly done

--- src/main.js
+++ src/main.js
@@ -1,1 +1,68 @@
-f
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+import { createImagesMarkup } from "./js/render-functions";
+import { getImages } from "./js/pixabay-api.js";
+
+import error from './img/error.png'
+
+
+const form = document.querySelector('.form');
+const imageContainer = document.querySelector('.images-container');
+const loader = document.querySelector('span');
+
+form.addEventListener("submit", submitHandler);
+
+function submitHandler(event) {
+  event.preventDefault();
+  
+  imageContainer.innerHTML = null;
+
+  const inputValue = form.input.value.trim();
+ 
+  if (inputValue) {
+    loader.classList.add('loader');
+    getImages(inputValue)
+      .then(({ hits }) => {
+        if (!hits.length) {
+          iziToast.error({
+            title: 'Error',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            iconUrl: error,
+            fontSize: 'large',
+            position:	'topRight',
+            messageColor: 'white',
+            titleColor: 'white',
+            theme: 'dark',
+            timeout:	5000,
+            backgroundColor: '#EF4040',
+            progressBar: false,
+          });
+        }
+        createImagesMarkup(hits);
+        loader.classList.remove('loader');
+      })
+  } else {
+    iziToast.error({
+      title: 'Error',
+      message:
+      'Sorry, there are no images matching your search query. Please try again!',
+      iconUrl: error,
+      fontSize: 'large',
+      position:	'topRight',
+      messageColor: 'white',
+      titleColor: 'white',
+      theme: 'dark',
+      timeout:	5000,
+      backgroundColor: '#EF4040',
+      progressBar: false,
+    });
+
+  }
+}
